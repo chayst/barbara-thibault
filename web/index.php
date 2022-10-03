602c2c3c1b6cc9984360ba13f4ec0b8d4ba73cd3
chayst <thibault.chays@gmail.com>
2022-10-03 23:08:32 +0200

    fixed handler for comments

--- web/index.php
+++ web/index.php
@@ -175,7 +175,7 @@ $app->get('/com/', function() use($app) {
     $comments[] = $commentsStatement;
   }
 
-  return $app['twig']->render('comment.twig', array(
+  return $app['twig']->render('comments.twig', array(
     'content' => $comments['content'],
     'author' => $comments['author'],
     'likes' => $comments['likes'],
